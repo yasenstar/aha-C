c74bf7fd3ef87f4e10732e9921089e0a6117ee6c
Zhao Xiaoqi <xiaoqi.zhao@volvo.com>
2023-12-31 20:31:58 -0500

    add ch04 initial

---
+++ 04_重量级选手登场/04-01_永不停止的哭声/04-01_1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	// while (1>0) printf("wa ");
+	system("pause");
+	return 0;
+}
---
+++ 04_重量级选手登场/04-01_永不停止的哭声/04-01_2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	system("color 0a");
+    // while (1) printf("0 1 ");
+	system("pause");
+	return 0;
+}
