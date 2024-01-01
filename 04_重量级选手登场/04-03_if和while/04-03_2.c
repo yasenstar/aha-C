65ff5d655dbbd4e2fc7366c4575340b9fba0fb7c
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-01-01 15:55:51 -0500

    add 4-03

---
+++ 04_重量级选手登场/04-03_if和while/04-03_2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int a;
+    a = 1;
+    while (a<=100)
+    {
+		if (a % 3 == 0 && a % 5 != 0)
+        {
+			printf("%d ", a);
+        }
+        a = a + 1;
+    }
+	system("pause");
+	return 0;
+}
