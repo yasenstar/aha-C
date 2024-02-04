5163bd89b41819fef1b22976a3a22e038773f1a0
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-02-04 09:11:42 -0500

    add 5-6(1)

---
+++ 5_好戏在后面/5-6_哥德巴赫猜想/5-6_1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int k, i, count1, j, count2, a, b;
+    for (k = 4; k <= 10000; k = k +2)
+    {
+		for (a = 2; a <= k / 2; a++)
+        {
+			count1 = 0;
+			for (i = 2; i<=a/2; i++)
+            {
+				if (a % i == 0) {
+					count1++;
+                    break;
+                }
+            }
+            if (count1 == 0) {
+				b = k - a;
+                count2 = 0;
+				for (j = 2; j<=b/2; j++)
+				{
+					if (b % j == 0) {
+						count2++;
+						break;
+					}
+				}
+                if (count2==0) {
+					printf("%d=%d+%d  ",k, a, b);
+                    break;
+                }
+            }
+        }
+    }
+	system("pause");
+	return 0;
+}
